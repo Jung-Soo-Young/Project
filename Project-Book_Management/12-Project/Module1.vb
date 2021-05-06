a6aa088699c77c9963010442f30c6794f413021d
Jung-Soo-Young <81272667+Jung-Soo-Young@users.noreply.github.com>
2021-05-06 10:33:39 +0900

    도서 관리 프로그램

---
+++ Project-Book_Management/12-Project/Module1.vb
@@ -0,0 +1,16 @@
+﻿Module Module1
+    Public Ret As String
+    Public SQL As String
+    Public Con As New OleDb.OleDbConnection
+    Public DCom As New OleDb.OleDbCommand
+    Public DA As New OleDb.OleDbDataAdapter
+
+    Public Sub DB_Access()
+        Dim My_con As String
+        My_con = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\library.accdb"
+
+        Con.ConnectionString = My_con
+        DCom.Connection = Con
+        Con.Open()
+    End Sub
+End Module
